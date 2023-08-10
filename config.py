78bffa4e9b107f5e8434afd7595aded30cebd3bd
Roman <Roman.vladimirovich23@gmail.com>
2023-08-10 17:47:52 +0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,12 @@
+TOKEN = "6396745214:AAFI74QUOC3Nssl_Smh4rV3YLs1UzvQ1YSQ"
+
+# Название бота - Vaultsbot_bot
+
+keys = {
+    "доллар": 'USD',
+    "биткоин": 'BTC',
+    "эфириум": 'ETH',
+    "евро": 'EUR',
+    "тенге": 'KZ',
+    "рубль": 'RUB'
+}
